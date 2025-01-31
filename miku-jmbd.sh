363f2282c9248dd9f59d5658bf902f580403a518
Adam YD <adamyd18@gmail.com>
2025-02-01 04:54:36 +0700

    Update miku-jmbd.sh

--- miku-jmbd.sh
+++ miku-jmbd.sh
@@ -8,12 +8,13 @@ rm -rf packages/apps/Launcher3
 # Initialize repo
 repo init -u https://github.com/XeroMz69/manifesto -b Vampire_v2
 git clone https://github.com/XeroMz69/manifest-lokal.git -b vampire .repo/local_manifests
+git clone https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b prebuilts/clang/host/linux-x86/clang-los --depth 1
 
 # Sync repos
 /opt/crave/resync.sh
 
 # Export environment variables
-export TARGET_RELEASE=ap3a
+export TARGET_RELEASE=ap4a
 export BUILD_USERNAME=Xero
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
@@ -25,7 +26,7 @@ export TZ=Asia/Jakarta
 source build/envsetup.sh
 
 # Lunch
-lunch miku_earth-ap3a-userdebug
+lunch miku_earth-ap4a-userdebug
 
 # Clean and build
 make installclean
