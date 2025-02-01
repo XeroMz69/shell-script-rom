e510267e118a3071dce769ae6e7f56ffb1d95985
Adam YD <adamyd18@gmail.com>
2025-02-01 10:28:40 +0700

    Update miku-jmbd.sh

--- miku-jmbd.sh
+++ miku-jmbd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+CLANG_DLINK="https://github.com/XeroMz69/Clang/releases/download/Xero-Clang-20.0.0git-20250113/Xero-Clang-20.0.0git-20250113.tar.gz"
+WORKDIR="$(pwd)"
+
 # Clean up old manifests and repos
 rm -rf .repo/local_manifests
 rm -rf frameworks/base
@@ -8,10 +11,15 @@ rm -rf packages/apps/Launcher3
 # Initialize repo
 repo init -u https://github.com/XeroMz69/manifesto -b Vampire_v2
 git clone https://github.com/XeroMz69/manifest-lokal.git -b vampire .repo/local_manifests
-git clone https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b prebuilts/clang/host/linux-x86/clang-los --depth 1
 
 # Sync repos
 /opt/crave/resync.sh
+
+# Some fixes
+mkdir -p $WORKDIR/prebuilts/prebuilts/clang/host/linux-x86/clang-los
+wget $CLANG_DLINK
+tar -C $WORKDIR/prebuilts/clang/host/linux-x86/clang-los/ -zxvf Xero-Clang-20.0.0git-20250113.tar.gz
+rm -rf Xero-Clang-20.0.0git-20250113.tar.gz
 
 # Export environment variables
 export TARGET_RELEASE=ap4a
