4c05c6f9ddc648d5e33170526549279cf51b488b
Adam YD <adamyd18@gmail.com>
2025-07-08 22:25:38 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create USER
+user=xero
+password=xero
+sudo useradd -m -s /bin/bash -G users xero
+echo "$user:$password" | chpasswd
+sudo echo "xero ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers
+
+# Configure SSH
+sudo apt install openssh-server -y
+sudo service ssh start
