ef35c0c2348bd4351379d5e9a94148d6a769ecba
Adam YD <adamyd18@gmail.com>
2025-07-21 22:14:24 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ user=xero
 password=xero
 sudo useradd -m -s /bin/bash -G users xero
 sudo echo "$user:$password" | sudo chpasswd
-sudo echo "xero ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers
+sudo echo "xero ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 # Configure SSH
 sudo apt update -y
@@ -17,3 +17,13 @@ sudo dpkg -i cloudflared*
 echo "Please enter the cloudflared token command:"
 read token
 $token
+
+# Configure Web Server
+sudo apt install apache2 -y
+sudo service apache2 start
+
+# Configure code-server
+sudo su -c 'wget https://github.com/coder/code-server/releases/download/v4.102.1/code-server_4.102.1_amd64.deb ;
+ sudo dpkg -i code-server* ;
+ sed -i.bak 's/auth: password/auth: none/' ~/.config/code-server/config.yaml ;
+ code-server --bind-addr 0.0.0.0:2000' -s /bin/bash xero
